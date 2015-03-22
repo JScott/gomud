5202608b264c22d60f39761249809a385fc493c4
JScott <jvscott@gmail.com>
2015-03-21 20:21:50 -0700

    Replacing the file I broke

--- server.go
+++ server.go
@@ -4,28 +4,29 @@ import (
 	"fmt"
 	"time"
 
-	zmq "github.com/alecthomas/gozmq"
 	"github.com/fiorix/go-redis/redis"
+	zmq "github.com/pebbe/zmq4"
 )
 
 const noFlags = 0
 
 func main() {
-	context, _ := zmq.NewContext()
-	socket, _ := context.NewSocket(zmq.REP)
-	defer context.Close()
-	defer socket.Close()
-	socket.Bind("tcp://*:5555")
+	server, _ := zmq.NewSocket(zmq.REP)
+	defer server.Close()
+	server.Bind("tcp://*:5555")
 
 	storage := redis.New("localhost:6379")
 
 	for {
-		msg, _ := socket.Recv(noFlags)
-		println("Received ", string(msg))
+		//  Wait for next request from client
+		msg, _ := server.Recv(noFlags)
+		fmt.Println("Received ", msg)
 
+		//  Do some 'work'
 		time.Sleep(time.Second)
 
-		reply := fmt.Sprintf("World")
-		socket.Send([]byte(reply), noFlags)
+		//  Send reply back to client
+		reply := "World"
+		server.Send(reply, noFlags)
 	}
 }
