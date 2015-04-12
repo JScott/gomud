cdd6123a4e7996b213c98a85d53b2e287817caff
JScott <jvscott@gmail.com>
2015-04-12 12:02:57 -0700

    Spaces are law in this repo

--- client.go
+++ client.go
@@ -1,75 +1,75 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strings"
+  "bufio"
+  "fmt"
+  "log"
+  "os"
+  "strings"
 
-	zmq "github.com/pebbe/zmq4"
-	"gomud/connections"
+  zmq "github.com/pebbe/zmq4"
+  "gomud/connections"
 )
 
 const usageErr = "client.go <user> <pass>"
 const authErr = "Credentials failed to log in"
 
 func main() {
-	//  Socket to talk to server
-	fmt.Println("Connecting...")
-	client, err := connections.CreateClient()
-	defer client.Close()
-	if err == nil {
-		loginAndPlay(client)
-	} else {
-		log.Fatal(err)
-	}
+  //  Socket to talk to server
+  fmt.Println("Connecting...")
+  client, err := connections.CreateClient()
+  defer client.Close()
+  if err == nil {
+    loginAndPlay(client)
+  } else {
+    log.Fatal(err)
+  }
 }
 
 func loginAndPlay(client *zmq.Socket) {
-	token, err := requestLoginWith(client)
-	if err == nil {
-		readCommandsInto(client, token)
-	} else {
-		log.Fatal(err)
-	}
+  token, err := requestLoginWith(client)
+  if err == nil {
+    readCommandsInto(client, token)
+  } else {
+    log.Fatal(err)
+  }
 }
 
 func requestLoginWith(client *zmq.Socket) (string, error) {
-	if len(os.Args) < 2 {
-		log.Fatal(usageErr)
-	}
-	username := os.Args[1]
-	//password := os.Args[2]
+  if len(os.Args) < 2 {
+    log.Fatal(usageErr)
+  }
+  username := os.Args[1]
+  //password := os.Args[2]
   return connections.RequestLogin(username, "", client)
 }
 
 func readCommandsInto(client *zmq.Socket, authToken string) {
-	stream := bufio.NewScanner(os.Stdin)
-	for stream.Scan() {
-		handlePlayerInput(stream.Text(), authToken, client)
-	}
-	err := stream.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+  stream := bufio.NewScanner(os.Stdin)
+  for stream.Scan() {
+    handlePlayerInput(stream.Text(), authToken, client)
+  }
+  err := stream.Err()
+  if err != nil {
+    log.Fatal(err)
+  }
 }
 
 func handlePlayerInput(input string, authToken string, client *zmq.Socket) {
-	command, body := parseInput(input)
-	reply, err := connections.RequestAction(command, body, authToken, client)
-	if err == nil {
-		fmt.Println("Received ", reply)
-	} else {
-		log.Fatal(err)
-	}
+  command, body := parseInput(input)
+  reply, err := connections.RequestAction(command, body, authToken, client)
+  if err == nil {
+    fmt.Println("Received ", reply)
+  } else {
+    log.Fatal(err)
+  }
 }
 
 func parseInput(input string) (command string, body string) {
   split := strings.SplitN(input, " ", 2)
   command := split[0]
   body := ""
-	if len(split) > 1 {
-		body = split[1]
-	}
+  if len(split) > 1 {
+    body = split[1]
+  }
 }
--- server.go
+++ server.go
@@ -1,41 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"log"
-	//"errors"
+  "fmt"
+  "time"
+  "log"
+  //"errors"
 
-	"gomud/connections"
-	//"encoding/json"
-	//"github.com/fiorix/go-redis/redis"
-	zmq "github.com/pebbe/zmq4"
+  "gomud/connections"
+  //"encoding/json"
+  //"github.com/fiorix/go-redis/redis"
+  zmq "github.com/pebbe/zmq4"
 )
 
 const noFlags = 0
 
 func main() {
-	server, err := connections.CreateServer()
-	defer server.Close()
-	if err == nil {
-		waitForPlayersOn(server)
-	} else {
-		log.Fatal(err)
-	}
+  server, err := connections.CreateServer()
+  defer server.Close()
+  if err == nil {
+    waitForPlayersOn(server)
+  } else {
+    log.Fatal(err)
+  }
 }
 
 func waitForPlayersOn(server *zmq.Socket) {
-	//storage := redis.New("localhost:6379")
-	for {
-		//  Wait for next request from client
-		msg, _ := server.Recv(noFlags)
-		fmt.Println("Received ", msg)
+  //storage := redis.New("localhost:6379")
+  for {
+    //  Wait for next request from client
+    msg, _ := server.Recv(noFlags)
+    fmt.Println("Received ", msg)
 
-		//  Do some 'work'
-		time.Sleep(time.Second)
+    //  Do some 'work'
+    time.Sleep(time.Second)
 
-		//  Send reply back to client
-		reply := "World"
-		server.Send(reply, noFlags)
-	}
+    //  Send reply back to client
+    reply := "World"
+    server.Send(reply, noFlags)
+  }
 }
